45a9574a13de4b946028d55573abaf613f65f244
George Snyder <gdsnyder@fedins.com>
2025-06-13 11:32:30 -0500

    readme changes, sync fixes, service-worker improvements

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A simple time-tracking application for your browser that persists to local storage or optionally webdav (with CORS-enabled).
 
 ![screenshot](docs/screenshot.png)
+
+It's one editable file (with CDN dependencies)!  While this isn't great for performance, I personally really like that anyone can downlod the file, run it, and edit/tweak it, without any sort of special developer setup or build tooling.  Of course, there is the service worker and manifest, but those aren't vital, and it doesn't seem like there is a way to embed these in the file.
--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,12 @@ const URLS_TO_CACHE = [
   'https://unpkg.com/react@17/umd/react.development.js',
   'https://unpkg.com/react-dom@17/umd/react-dom.development.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css'
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css',
+  'https://snydergd.github.io/images/paper.jpg',
+];
+const WEB_FIRST_URLS = [
+  /.*\/time2.html$/,
+  /.*\/manifest.json$/,
 ];
 
 // Install event - cache specified resources
@@ -18,14 +23,14 @@ self.addEventListener('install', event => {
 
 // Fetch event - handle caching logic
 self.addEventListener('fetch', event => {
-  const request = request;
+  const request = event.request;
   const url = new URL(request.url);
 
   const isFont = /\.(woff2?|ttf|otf|eot)$/i.test(url.pathname);
   const isCachedResource = URLS_TO_CACHE.includes(request.url);
-  const isTime2Html = url.pathname.endsWith('time2.html');
+  const isWebFirstUrl = WEB_FIRST_URLS.filter(x => x.test(request.url)).length > 0;
 
-  if (isTime2Html) {
+  if (isWebFirstUrl) {
     event.respondWith(
       fetch(request)
         .then(networkResponse => {
